86fb2c0623e737c644412307b9b2b241786c0dfa
hbzhang <hbzhang@mtsu.edu>
2023-04-25 05:10:13 +0000

    Change output name for dataset run script.

--- run-on-datasets.sh
+++ run-on-datasets.sh
@@ -1,5 +1,5 @@
 for FILE in data/*.csv
 do
     echo $(basename $FILE)
-    python other_script.py --data-file-name $(basename $FILE) > $(basename $FILE).log
+    python other_script.py --data-file-name $(basename $FILE) > ./dropout-high/$(basename $FILE).log
 done
